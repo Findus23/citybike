795c4eba9c6cd163b9f60600d472f256fb26e7e7
Lukas Winkler <l.winkler23@mailbox.org>
2016-05-31 14:50:05 +0200

    Länge anhand von GPX-Datei

--- iterations.py
+++ iterations.py
@@ -2,10 +2,11 @@
 
 import itertools
 import os
-import re
+import xml.dom.minidom
 from pprint import pprint
-
 import MySQLdb
+
+from gpxlen import getTrackLength
 
 db = MySQLdb.connect(host="localhost",  # your host, usually localhost
                      user="root",  # your username
@@ -27,15 +28,15 @@ for way in itertools.combinations(list, 2):
     success = os.system(command)
     if success == 0:
         os.rename("quickest-track.gpx", "file/" + str(i) + ".gpx")
-        with open('quickest.txt') as f:
-            lines = f.readlines()
-            last = lines[-1]
-            duration = re.findall("(\d+) min", last)[1]
-            length = re.findall("(\d+\.\d+) km", last)[1]
-            length = re.findall("(\d+\.\d+) km", last)[1]
-        cur.execute("REPLACE INTO connections (id, start, goal, length, duration) VALUES (%s,%s,%s,%s,%s)",
-                    (i, way[0][2], way[1][2], length, duration))
+        dom = xml.dom.minidom.parse("file/" + str(i) + ".gpx")
+        gpxNode = dom.firstChild
+        length = round(getTrackLength(gpxNode.getElementsByTagName("trk")[0]), 0)
+
+        cur.execute("REPLACE INTO connections (id, start, goal, length) VALUES (%s,%s,%s,%s)",
+                    (i, way[0][2], way[1][2], length))
         db.commit()
+    else:
+        print("   " + str(way[0][2]) + "   " + str(way[1][2]))
     print(i)
     i += 1
 db.commit()
