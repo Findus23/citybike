159978a650df48b845f1aae8f7a1869cdb3bf3ba
Lukas Winkler <l.winkler23@mailbox.org>
2016-07-18 10:39:08 +0200

    routing.py überarbeitet

--- routing.py
+++ routing.py
@@ -2,10 +2,11 @@
 
 import itertools
 import os
+import subprocess
 import xml.dom.minidom
-from pprint import pprint
 
 import MySQLdb
+from tqdm import tqdm
 
 from config import database
 from gpxlen import getTrackLength
@@ -17,28 +18,40 @@ db = MySQLdb.connect(database["host"],
 
 cursor = db.cursor()
 
-cursor.execute("SELECT lat, lon,ref FROM stationen ORDER BY ref DESC")
+cursor.execute("SELECT lat, lon,ref FROM stationen ORDER BY ref DESC")  # Liste der Stationen
 stations = cursor.fetchall()
-id = 1
-for way in itertools.combinations(stations, 2):
-    command = "routino-router --dir=/home/lukas/routino/data" \
-              " --lat1={}".format(way[0][0]) + \
-              " --lon1={}".format(way[0][1]) + \
-              " --lat2={}".format(way[1][0]) + \
-              " --lon2={}".format(way[1][1]) + \
-              " --quickest --transport=bicycle --output-gpx-track --quiet"
-    success = os.system(command)
-    if success == 0:
-        os.rename("quickest-track.gpx", "file/" + str(id) + ".gpx")
-        dom = xml.dom.minidom.parse("file/" + str(id) + ".gpx")
-        gpxNode = dom.firstChild
-        length = round(getTrackLength(gpxNode.getElementsByTagName("trk")[0]), 0)
+routeID = 1
+totalCombinations = 7260  # 212 über 2
+pbar = tqdm(total=totalCombinations)
 
-        # cursor.execute("REPLACE INTO connections (id, start, goal, length) VALUES (%s,%s,%s,%s)",
-        #                (id, way[0][2], way[1][2], length))
-    else:
-        print("   " + str(way[0][2]) + "   " + str(way[1][2]))
-        print(command)
-    # print(id)
-    id += 1
+for way in itertools.combinations(stations, 2):
+    command = [
+        "routino-router",
+        "--dir=/home/lukas/routino/data",
+        "--lat1={}".format(way[0][0]),
+        "--lon1={}".format(way[0][1]),
+        "--lat2={}".format(way[1][0]),
+        "--lon2={}".format(way[1][1]),
+        "--quickest",
+        "--transport=bicycle",
+        "--output-gpx-track",
+        "--quiet"
+    ]
+    try:
+        subprocess.check_output(command, stderr=subprocess.STDOUT)
+        os.rename("quickest-track.gpx", "file/" + str(routeID) + ".gpx")  # nach file/2.gpx verschieben
+        dom = xml.dom.minidom.parse("file/" + str(routeID) + ".gpx")
+        gpxNode = dom.firstChild
+        length = round(getTrackLength(gpxNode.getElementsByTagName("trk")[0]), 0)  # Länge aus gpx auslesen
+
+        cursor.execute("REPLACE INTO connections (id, start, goal, length) VALUES (%s,%s,%s,%s)",
+                       (routeID, way[0][2], way[1][2], length))  # in db eintragen
+    except subprocess.CalledProcessError as exception:
+        print()  # Neue Zeile
+        print("Fehler zwischen Station " + str(way[0][2]) + " und " + str(way[1][2]))
+        print(exception.output.decode())
+    pbar.update(1)
+    # stdout.write("\r{:.4f}".format(routeID / totalCombinations * 100))  # Prozentanzeige
+    routeID += 1
+pbar.close()
 db.commit()
