d27c88c949010dc5cc9f7dc5ee26e519598f557e
Lukas Winkler <l.winkler23@mailbox.org>
2016-10-06 09:01:31 +0200

    Tabelle verbessert

--- www/sidebar.js
+++ www/sidebar.js
@@ -7,37 +7,40 @@ var sidebar = L.Control.extend({
         container.style.backgroundColor = "white";
         container.style.height = '600px';
         container.style.width = '400px';
-        var table = L.DomUtil.create("table", "sidebarTable", container);
         var button = document.createElement("button");
         button.textContent = "Topliste";
         container.appendChild(button);
+
+        var table = L.DomUtil.create("table", "sidebarTable pure-table pure-table-horizontal", container);
+
+        button.addEventListener("click", function () {
+            loadSidebar(table);
+        });
+        loadSidebar(table);
+
 
         return container;
     }
 });
 map.addControl(new sidebar);
 
-$(".leaflet-control-custom button").on("click", function () {
-    loadSidebar();
-});
-
-function loadSidebar() {
+function loadSidebar(table) {
     $.getJSON("/api/top/", {
         type: "shortestConnections"
     }).done(function (data) {
-        var table = $(".sidebarTable");
+        while (table.firstChild) {
+            table.removeChild(table.firstChild);
+        }
         for (var i = 0; i < data.length; i++) {
             var line = document.createElement("tr");
             var single = data[i];
-            console.log(single);
             for (var j = 0; j < single.length; j++) {
                 var td = document.createElement("td");
                 td.textContent = single[j];
                 line.appendChild(td);
             }
-            table.append(line);
+            table.appendChild(line);
         }
-
     });
 
 }
